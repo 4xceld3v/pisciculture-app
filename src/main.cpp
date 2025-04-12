bf27ea89d670cf21829a284eaa930b9b94d607bb
cristian bambague <axceldev98@gmail.com>
2025-04-12 10:55:22 -0500

    feat(app): capture temp and humidity code

--- src/main.cpp
+++ src/main.cpp
@@ -1,69 +1,88 @@
 #include <WiFi.h>
-#include <Wire.h>
-#include <LiquidCrystal_I2C.h>
+#include <PubSubClient.h>
+#include <DHT.h>
 
-LiquidCrystal_I2C LCD = LiquidCrystal_I2C(0x27, 16, 2);
+// WiFi
+const char* ssid = "Wokwi-GUEST";
+const char* password = "";
 
-#define NTP_SERVER     "pool.ntp.org"
-#define UTC_OFFSET     0
-#define UTC_OFFSET_DST 0
+// MQTT
+const char* mqtt_server = "broker.mqttdashboard.com";
+const char* mqtt_topic = "wokwi-weather";
+const char* mqtt_client_id = "esp32-weather-demo";
 
-void spinner() {
-  static int8_t counter = 0;
-  const char* glyphs = "\xa1\xa5\xdb";
-  LCD.setCursor(15, 1);
-  LCD.print(glyphs[counter++]);
-  if (counter == strlen(glyphs)) {
-    counter = 0;
+// DHT
+#define DHTPIN 15
+#define DHTTYPE DHT22
+DHT dht(DHTPIN, DHTTYPE);
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+String prevWeather = "";
+
+void setup_wifi() {
+  delay(10);
+  Serial.print("Connecting to WiFi");
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(100);
+    Serial.print(".");
   }
+  Serial.println(" Connected!");
 }
 
-void printLocalTime() {
-  struct tm timeinfo;
-  if (!getLocalTime(&timeinfo)) {
-    LCD.setCursor(0, 1);
-    LCD.println("Connection Err");
-    return;
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Connecting to MQTT server... ");
+    if (client.connect(mqtt_client_id)) {
+      Serial.println("Connected!");
+    } else {
+      Serial.print("Failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000);
+    }
   }
-
-  LCD.setCursor(8, 0);
-  LCD.println(&timeinfo, "%H:%M:%S");
-
-  LCD.setCursor(0, 1);
-  LCD.println(&timeinfo, "%d/%m/%Y   %Z");
 }
 
 void setup() {
   Serial.begin(115200);
-
-  LCD.init();
-  LCD.backlight();
-  LCD.setCursor(0, 0);
-  LCD.print("Connecting to ");
-  LCD.setCursor(0, 1);
-  LCD.print("WiFi ");
-
-  WiFi.begin("Wokwi-GUEST", "", 6);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(250);
-    spinner();
-  }
-
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-
-  LCD.clear();
-  LCD.setCursor(0, 0);
-  LCD.println("Online");
-  LCD.setCursor(0, 1);
-  LCD.println("Updating time...");
-
-  configTime(UTC_OFFSET, UTC_OFFSET_DST, NTP_SERVER);
+  dht.begin();
+  //setup_wifi();
+  //client.setServer(mqtt_server, 1883);
 }
 
 void loop() {
-  printLocalTime();
-  delay(250);
+  /*if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();*/
+
+  Serial.print("Measuring weather conditions... ");
+  float temperature = dht.readTemperature();
+  float humidity = dht.readHumidity();
+
+  if (isnan(temperature) || isnan(humidity)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  String payload = "{\"temp\":" + String(temperature, 1) + ",\"humidity\":" + String(humidity, 1) + "}";
+
+  Serial.println(payload);
+  
+  /*if (payload != prevWeather) {
+    Serial.println("Updated!");
+    Serial.print("Reporting to MQTT topic ");
+    Serial.print(mqtt_topic);
+    Serial.print(": ");
+    Serial.println(payload);
+    client.publish(mqtt_topic, payload.c_str());
+    prevWeather = payload;
+  } else {
+    Serial.println("No change");
+  }*/
+
+  delay(1000);
 }
